754ce8df1df3abe7c8f1d7538698032ea707866e
Valentin Lukyanets <valikluks95@gmail.com>
2023-04-22 19:48:00 +0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function CustomInput({ id, type, placeholder, checking, predicates, setValid }) {
-  const [value, setValue] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+function CustomInput({
+  id,
+  type,
+  placeholder,
+  checking,
+  predicates,
+  setValid,
+}) {
+  const [value, setValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [inFocus, setInFocus] = useState(false);
-  const [lastErrorMessage, setLastErrorMessage] = useState('');
+  const [lastErrorMessage, setLastErrorMessage] = useState("");
 
   function makeChange(newValue) {
     setValue(newValue);
@@ -38,37 +45,42 @@ function CustomInput({ id, type, placeholder, checking, predicates, setValid }) 
     checking && setLastErrorMessage("");
   }
 
+  const actualErrorMessage =
+    checking && errorMessage !== ""
+      ? errorMessage
+      : inFocus && lastErrorMessage !== "" && lastErrorMessage;
+
   return (
-    <>
-      <input 
-        placeholder={placeholder} 
-        id={id} 
+    <div className={"input-container" + (actualErrorMessage ? " invalid" : "")}>
+      <input
+        placeholder={placeholder}
+        id={id}
         type={type}
-        value={value} 
+        value={value}
         onChange={(e) => makeChange(e.target.value)}
         onFocus={onFocus}
         onBlur={onBlur}
       />
-      {checking && errorMessage !== "" ? 
-        <label htmlFor={id}>{errorMessage}</label> :
-        inFocus && lastErrorMessage !== "" && <label htmlFor={id}>{lastErrorMessage}</label>
-      }
-    </>
-  )
+      <img src="/icon-error.svg" alt="" />
+      {actualErrorMessage && <label htmlFor={id}>{actualErrorMessage}</label>}
+    </div>
+  );
 }
 
 function App() {
   const [checking, setChecking] = useState(false);
-  const [validFirstName, setValidFirstName] = useState(false);
-  const [validLastName, setValidLastName] = useState(false);
-  const [validEmail, setValidEmail] = useState(false);
-  const [validPassword, setValidPassword] = useState(false);
+  const [, setValidFirstName] = useState(false);
+  const [, setValidLastName] = useState(false);
+  const [, setValidEmail] = useState(false);
+  const [, setValidPassword] = useState(false);
 
   function claimHandler() {
     setChecking(true);
-    if (validFirstName && validLastName && validEmail && validPassword) {
-      console.log("form submit");
-    }
+  }
+
+  function validateEmail(email) {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email);
   }
 
   return (
@@ -76,57 +88,81 @@ function App() {
       <div className="left-side">
         <div className="all-text white">
           <p className="header-text bold">Learn to code by watching others</p>
-          <p className="detail-text">See how experienced developers solve problems in real&#8209;time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+          <p className="detail-text">
+            See how experienced developers solve problems in real&#8209;time.
+            Watching scripted tutorials is great, but understanding how
+            developers think is invaluable.
+          </p>
         </div>
       </div>
       <div className="right-side">
-        <div className="try-free-box blue-bg bottom-shadow text-center-x">
+        <div className="try-free-box blue-bg bottom-shadow">
           <div className="container">
             <span className="bold">Try it free 7 days</span>
             <span> then $20/mo. thereafter</span>
           </div>
         </div>
         <form className="signup-form white-bg bottom-shadow">
-          <CustomInput 
+          <CustomInput
             placeholder="First Name"
             id="first-name"
             type="text"
-            predicates={[{predicate: (value) => value.length > 3, text: "First Name cannot be empty"}]}
+            predicates={[
+              {
+                predicate: (value) => value.length > 0,
+                text: "First Name cannot be empty",
+              },
+            ]}
             checking={checking}
             setValid={setValidFirstName}
           />
           <CustomInput
-            placeholder="Last Name" 
+            placeholder="Last Name"
             id="last-name"
             type="text"
-            predicates={[{predicate: (value) => value.length > 3, text: "Last Name cannot be empty"}]}
+            predicates={[
+              {
+                predicate: (value) => value.length > 0,
+                text: "Last Name cannot be empty",
+              },
+            ]}
             checking={checking}
             setValid={setValidLastName}
           />
-          <CustomInput 
-            placeholder="Email Address" 
+          <CustomInput
+            placeholder="Email Address"
             id="email-address"
-            type="text"
-            predicates={[{predicate: (value) => value.length > 3, text: "Looks like this is not an email"}]}
+            type="email"
+            predicates={[
+              {
+                predicate: validateEmail,
+                text: "Looks like this is not an email",
+              },
+            ]}
             checking={checking}
             setValid={setValidEmail}
           />
-          <CustomInput 
+          <CustomInput
             placeholder="Password"
             id="password"
             type="password"
-            predicates={[{predicate: (value) => value.length > 3, text: "Password cannot be empty"}]}
+            predicates={[
+              {
+                predicate: (value) => value.length > 0,
+                text: "Password cannot be empty",
+              },
+            ]}
             checking={checking}
             setValid={setValidPassword}
           />
-          <div 
+          <div
             className="claim-free-trial green-bg white clickable"
             onClick={claimHandler}
           >
             <span className="non-selectable">CLAIM YOUR FREE TRIAL</span>
           </div>
-          <div className="terms-and-services text-center-x">
-            <span className="grayish-blue">By clicking the button, you are agreeing to our </span>
+          <div className="terms-and-services">
+            <span className="grayish-blue">By clicking the button, you are agreeing to our</span>
             <span className="bold red clickable">Terms and Services</span>
           </div>
         </form>
